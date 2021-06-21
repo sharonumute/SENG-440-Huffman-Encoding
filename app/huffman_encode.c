73407dcdbf209d200f20e92ae23cf84aa629bfa1
Sharon Umute <sharonumute@gmail.com>
2021-06-21 16:21:52 -0700

    draft encoding and decoding

--- app/huffman_encode.c
+++ app/huffman_encode.c
@@ -1,9 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <string.h>
 
 #include "../include/huffman_encode.h"
 
+char** encode_word(int target){
+    char ** encoded= malloc(130 * sizeof(char*));
+    for (size_t i = 0; i < target; i++)
+    {
+        strcat(encoded, "1");
+    }
+    strcat(encoded, "0");
+	return encoded;
+}
 
+char** decode_sentence(char * target){
+    char * t;
+    char ** decoded= malloc(128 * sizeof(char*));
+
+    int count = 0;
+    for (t = target; *t != '\0'; t++) {
+        if (*t == '1'){
+            count++;
+        }else if (*t == '0'){
+            char buffer[3];
+            sprintf(buffer, "%d", count);
+            strcat(decoded, buffer);
+            count = 0;
+        }
+    }
+
+    return decoded;
+}
 
 int main() {
 
@@ -16,6 +45,7 @@ int main() {
 
     printf("sum: %1.100Lf\n", sum_of_p);
 
-    
+    printf("%s\n", encode_word(127));
+    printf("%s\n", decode_sentence("0101100"));
     return 0;
 }
